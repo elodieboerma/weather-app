c9a3f35835a29bae59422f0ee6167e1d76accedd
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-01 22:14:35 -0500

    specify which data to use

--- src/index.js
+++ src/index.js
@@ -38,10 +38,36 @@ function processData () {
 function chooseData() {
     const fullData = processData();
 
-    const temp = fullData.days[0].temp;
-    
+    const forecastDescrip = fullData.description;
 
-    const neededData = {temp: temp};
+    const temp = fullData.days[0].temp;
+    const minTemp = fullData.days[0].tempmin;
+    const maxTemp = fullData.days[0].tempmax;
+    const feelsLike = fullData.days[0].feelslike;
+    const precip = fullData.days[0].precip;
+    const precipType = fullData.days[0].preciptype;
+    const snow = fullData.days[0].snow;
+    const snowfall = fullData.days[0].snowDepth;
+    const windGusts = fullData.days[0].windgust;
+    const windSpeed = fullData.days[0].windspeed;
+    const uvIndex = fullData.days[0].uvindex;
+    const sunrise = fullData.days[0].sunrise;
+    const sunset = fullData.days[0].sunset;
+    const dayDescrip = fullData.days[0].description;
+
+    const neededData = {forecast: forecastDescrip, temp: temp,minTemp: minTemp, maxTemp: maxTemp, 
+        feelsLike: feelsLike, precip: precip, windGusts: windGusts, windSpeed: windSpeed, uvIndex: uvIndex, 
+        sunrise: sunrise, sunset: sunset, dayDescrip: dayDescrip
+    };
+
+    if (precip !== 0) {
+        Object.defineProperty(neededData, "precipType", {value: precipType});
+        if (snow !== 0 && precipType === "snow") {
+            Object.defineProperty(neededData, "snow", {value: snow});
+            Object.defineProperty(neededData, "snowfall", {value: snowfall});
+        };
+    };
+
     return neededData;
 }
 
