a8c870dac4a472cbd23f2590580d56c2e3b98fa3
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-10 23:23:58 -0500

    change border colors based on temperature

--- src/displayDom.js
+++ src/displayDom.js
@@ -51,14 +51,15 @@ function formatData(data) {
 }
 
 export async function displayData(dataText) {
-  let data = JSON.parse(dataText);
-  data = await formatData(data);
+  let parsedData = JSON.parse(dataText);
+  let data = await formatData(parsedData);
 
   const div = document.createElement("div");
   div.id = "data";
 
   const subheadingDiv = document.createElement("h3");
   subheadingDiv.id = "subheading";
+  subheadingDiv.classList.add("border");
   const today = document.createElement("p");
   today.textContent = data.today;
   const forecast = document.createElement("p");
@@ -67,6 +68,7 @@ export async function displayData(dataText) {
 
   const tempDescripDiv = document.createElement("div");
   tempDescripDiv.id = "temps";
+  tempDescripDiv.classList.add("border");
   const temp = document.createElement("p");
   temp.textContent = data.temp;
   temp.id = "temp";
@@ -83,12 +85,14 @@ export async function displayData(dataText) {
 
   const precipationDiv = document.createElement("div");
   precipationDiv.id = "precipitation";
+  precipationDiv.classList.add("border");
   const precip = document.createElement("p");
   precip.textContent = data.precip;
   precipationDiv.append(precip);
 
   const windDiv = document.createElement("div");
   windDiv.id = "winds";
+  windDiv.classList.add("border");
   const windGusts = document.createElement("p");
   windGusts.textContent = data.windGusts;
   const windSpeed = document.createElement("p");
@@ -97,22 +101,52 @@ export async function displayData(dataText) {
 
   const uvDiv = document.createElement("div");
   uvDiv.id = "uv";
+  uvDiv.classList.add("border");
   const uvIndex = document.createElement("p");
   uvIndex.textContent = data.uvIndex;
   uvDiv.append(uvIndex);
 
   const sunriseDiv = document.createElement("div");
   sunriseDiv.id = "sunrise";
+  sunriseDiv.classList.add("border");
   const sunrise = document.createElement("p");
   sunrise.textContent = data.sunrise;
   sunriseDiv.append(sunrise);
 
   const sunsetDiv = document.createElement("div");
   sunsetDiv.id = "sunset";
+  sunsetDiv.classList.add("border");
   const sunset = document.createElement("p");
   sunset.textContent = data.sunset;
   sunsetDiv.append(sunset);
 
   div.append(subheadingDiv,tempDescripDiv, precipationDiv, windDiv, uvDiv, sunriseDiv, sunsetDiv);
   document.body.appendChild(div);
+  alignColor(parsedData.temp);
+}
+
+
+function alignColor(temp) {
+  const divList = document.getElementsByClassName("border");
+  const unit = document.getElementById("changeUnits").textContent;
+  let color;
+  if (temp < 20 && unit === "Fahrenheit" || temp < -7.2 && unit === "Celsius") {
+    color = "rgb(83, 83, 247)";
+  } else if (temp >= 20 && temp < 40 && unit === "Fahrenheit" || temp > -7.2 && temp < 4.4 && unit === "Celsius") {
+    color = "rgb(147, 243, 147)";
+  } else if (temp >= 40 && temp < 60 && unit === "Fahrenheit" || temp > 4.4 && temp < 15.5 && unit === "Celsius") {
+    color = "rgb(247, 247, 163)";
+  } else if (temp >= 60 && temp < 80 && unit === "Fahrenheit" || temp > 15.5 && temp < 26.6 && unit === "Celsius") {
+    color = "rgb(245, 200, 116)";
+  } else if (temp >= 80 && unit === "Fahrenheit" || temp > 26.6 && unit === "Celsius") {
+    color = "rgb(248, 103, 103)";
+  } else {
+    color = "black";
+  }
+
+  for (let i = 0; i < divList.length; i++) {
+    console.log(divList[i]);
+    console.log(color);
+    divList[i].style.borderColor = color;
+  }
 }
