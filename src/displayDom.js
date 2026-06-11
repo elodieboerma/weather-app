c55add24347c0189a1a1644404275ff2edd14892
elodieboerma <214750279+elodieboerma@users.noreply.github.com>
2026-06-10 22:05:14 -0500

    make divs more visible

--- src/displayDom.js
+++ src/displayDom.js
@@ -34,7 +34,8 @@ export function getLocation() {
 
 function formatData(data) {
   const dataText = {
-    forecast: `Forecast: ${data.dayDescrip} ${data.forecast}`,
+    today: `Today: ${data.dayDescrip}`,
+    forecast: `Later: ${data.forecast}`,
     temp: `Temperature: ${data.temp}°F`,
     minTemp: `Min temp: ${data.minTemp}°F`,
     maxTemp: `Max temp: ${data.maxTemp}°F`,
@@ -56,10 +57,16 @@ export async function displayData(dataText) {
   const div = document.createElement("div");
   div.id = "data";
 
-  const subHeading = document.createElement("h3");
-  subHeading.textContent = data.forecast;
+  const subheadingDiv = document.createElement("h3");
+  subheadingDiv.id = "subheading";
+  const today = document.createElement("p");
+  today.textContent = data.today;
+  const forecast = document.createElement("p");
+  forecast.textContent = data.forecast;
+  subheadingDiv.append(today,forecast);
 
   const tempDescripDiv = document.createElement("div");
+  tempDescripDiv.id = "temps";
   const temp = document.createElement("p");
   temp.textContent = data.temp;
   const minTemp = document.createElement("p");
@@ -71,11 +78,13 @@ export async function displayData(dataText) {
   tempDescripDiv.append(temp, minTemp, maxTemp, feelsLike);
 
   const precipationDiv = document.createElement("div");
+  precipationDiv.id = "precipitation";
   const precip = document.createElement("p");
   precip.textContent = data.precip;
   precipationDiv.append(precip);
 
   const windDiv = document.createElement("div");
+  windDiv.id = "winds";
   const windGusts = document.createElement("p");
   windGusts.textContent = data.windGusts;
   const windSpeed = document.createElement("p");
@@ -83,20 +92,23 @@ export async function displayData(dataText) {
   windDiv.append(windGusts, windSpeed);
 
   const uvDiv = document.createElement("div");
+  uvDiv.id = "uv";
   const uvIndex = document.createElement("p");
   uvIndex.textContent = data.uvIndex;
   uvDiv.append(uvIndex);
 
   const sunriseDiv = document.createElement("div");
+  sunriseDiv.id = "sunrise";
   const sunrise = document.createElement("p");
   sunrise.textContent = data.sunrise;
   sunriseDiv.append(sunrise);
 
   const sunsetDiv = document.createElement("div");
+  sunsetDiv.id = "sunset";
   const sunset = document.createElement("p");
   sunset.textContent = data.sunset;
   sunsetDiv.append(sunset);
 
-  div.append(subHeading,tempDescripDiv, precipationDiv, windDiv, uvDiv, sunriseDiv, sunsetDiv);
+  div.append(subheadingDiv,tempDescripDiv, precipationDiv, windDiv, uvDiv, sunriseDiv, sunsetDiv);
   document.body.appendChild(div);
 }
